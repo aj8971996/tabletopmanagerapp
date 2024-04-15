ff2a30df08de54cc83942cd6b48be73ac0f87b05
Alexander Barkus <alexander.barkus96@gmail.com>
2024-04-14 19:17:18 -0700

    Updated recycler view for species

--- app/src/main/java/com/app/homebrewed/CreateCharacterActivity.java
+++ app/src/main/java/com/app/homebrewed/CreateCharacterActivity.java
@@ -1,24 +1,40 @@
 package com.app.homebrewed;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class CreateCharacterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 1. Fetch String Array
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_character);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        String[] speciesArray = getResources().getStringArray(R.array.species);
+        if (speciesArray == null || speciesArray.length == 0) {
+            Log.e("CreateCharacterActivity", "Species array is empty or null!");
+            // Handle the error - maybe show a message to the user
+            return;
+        }
+
+        // 2. Find RecyclerView
+        RecyclerView speciesRecyclerView = findViewById(R.id.speciesRecyclerView);
+        Log.d("CreateCharacterActivity", "speciesRecyclerView: " + speciesRecyclerView);
+
+        // 3. Create and set Adapter
+        SpeciesListAdapter adapter = new SpeciesListAdapter(speciesArray);
+        speciesRecyclerView.setAdapter(adapter);
+
+        // 4. Set Layout Manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        speciesRecyclerView.setLayoutManager(layoutManager);
     }
 }
---
+++ app/src/main/java/com/app/homebrewed/SpeciesListAdapter.java
@@ -0,0 +1,48 @@
+package com.app.homebrewed;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SpeciesListAdapter extends RecyclerView.Adapter<SpeciesListAdapter.SpeciesViewHolder> {
+
+    private String[] speciesData; // Holds the data for your list
+
+    // Constructor
+    public SpeciesListAdapter(String[] speciesData) {
+        this.speciesData = speciesData;
+    }
+
+    // Inner class to define how a single list item looks
+    public static class SpeciesViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView1;
+
+        public SpeciesViewHolder(View itemView) {
+            super(itemView);
+            textView1 = itemView.findViewById(R.id.textView1);
+        }
+    }
+
+    // Required Methods (We'll fill them in)
+    @NonNull
+    @Override
+    public SpeciesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View listItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_character_species, parent, false);
+        return new SpeciesViewHolder(listItem); // Correct return statement
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SpeciesViewHolder holder, int position) {
+        String currentSpecies = speciesData[position];
+        holder.textView1.setText(currentSpecies);
+    }
+
+    @Override
+    public int getItemCount() {
+        return speciesData.length;
+    }
+}
