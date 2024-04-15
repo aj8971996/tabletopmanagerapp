faa3191e15b4a1b1abe8448d053e203b11b92eb3
Alexander Barkus <alexander.barkus96@gmail.com>
2024-04-14 20:19:08 -0700

    Added scroll indicator below species, and scroll snap

--- app/src/main/java/com/app/homebrewed/CreateCharacterActivity.java
+++ app/src/main/java/com/app/homebrewed/CreateCharacterActivity.java
@@ -2,14 +2,19 @@ package com.app.homebrewed;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 public class CreateCharacterActivity extends AppCompatActivity {
 
@@ -36,5 +41,50 @@ public class CreateCharacterActivity extends AppCompatActivity {
         // 4. Set Layout Manager
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         speciesRecyclerView.setLayoutManager(layoutManager);
+
+        // In your CreateCharacterActivity, after setting the LayoutManager:
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(speciesRecyclerView);
+
+        // Additional for indicator
+        final LinearLayout indicatorLayout = findViewById(R.id.indicatorLayout);
+        setupIndicatorDots(speciesArray.length, indicatorLayout);
+
+        speciesRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                updateIndicatorDots(layoutManager.findFirstVisibleItemPosition(), indicatorLayout);
+            }
+        });
+    }
+
+    private void setupIndicatorDots(int count, LinearLayout indicatorLayout) {
+        ImageView[] dots = new ImageView[count];
+        for (int i = 0; i < count; i++) {
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(getResources().getDrawable(R.drawable.unselected_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(8, 0, 8, 0);
+
+            indicatorLayout.addView(dots[i], params);
+        }
+
+        // Initially highlight the first dot
+        dots[0].setImageDrawable(getResources().getDrawable(R.drawable.selected_dot));
+    }
+
+    private void updateIndicatorDots(int position, LinearLayout indicatorLayout) {
+        int childCount = indicatorLayout.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView dot = (ImageView) indicatorLayout.getChildAt(i);
+            dot.setImageDrawable(getResources().getDrawable(i == position ? R.drawable.selected_dot : R.drawable.unselected_dot));
+        }
     }
 }
+
