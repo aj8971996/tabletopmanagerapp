622b73e123b1bbd73c0cfb005f585e398e7ce1b6
Alexander Barkus <alexander.barkus96@gmail.com>
2024-04-15 19:48:12 -0700

    Adding Abilities - Finished Dancer Class Abilities

--- app/src/main/java/com/app/homebrewed/Ability.java
+++ app/src/main/java/com/app/homebrewed/Ability.java
@@ -6,6 +6,7 @@ public class Ability {
     private String abilityClass;
     private String abilityName;
     private String abilityDescription;
+    private String abilityRange;
     private String abilityDamage;
     private String abilityEffect;
     public int getAbilityIndex() { return abilityIndex; }
@@ -13,6 +14,7 @@ public class Ability {
     public String getAbilityClass() { return abilityClass; }
     public String getAbilityName() { return abilityName; }
     public String getAbilityDescription() { return abilityDescription; }
+    public String getAbilityRange() { return abilityRange; }
     public String getAbilityDamage() { return abilityDamage; }
     public String getAbilityEffect() { return abilityEffect; }
     public void setAbilityIndex(Integer i) { this.abilityIndex = i; }
@@ -20,6 +22,7 @@ public class Ability {
     public void setAbilityClass(String s) { this.abilityClass = s; }
     public void setAbilityName(String s) { this.abilityName = s; }
     public void setAbilityDescription(String s) { this.abilityDescription = s; }
+    public void setAbilityRange(String s) { this.abilityDescription = s; }
     public void setAbilityDamage(String s) { this.abilityDamage = s; }
     public void setAbilityEffect(String s) { this.abilityEffect = s; }
 }
