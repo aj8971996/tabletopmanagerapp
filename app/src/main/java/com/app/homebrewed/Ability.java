ccf01e4cf5e6b8454516f5d8f0ff92400ca9f035
Alexander Barkus <alexander.barkus96@gmail.com>
2024-04-15 18:53:33 -0700

    Added Ability.java

---
+++ app/src/main/java/com/app/homebrewed/Ability.java
@@ -0,0 +1,25 @@
+package com.app.homebrewed;
+
+public class Ability {
+    private int abilityIndex;
+    private int abilityLevel;
+    private String abilityClass;
+    private String abilityName;
+    private String abilityDescription;
+    private String abilityDamage;
+    private String abilityEffect;
+    public int getAbilityIndex() { return abilityIndex; }
+    public int getAbilityLevel() { return abilityLevel; }
+    public String getAbilityClass() { return abilityClass; }
+    public String getAbilityName() { return abilityName; }
+    public String getAbilityDescription() { return abilityDescription; }
+    public String getAbilityDamage() { return abilityDamage; }
+    public String getAbilityEffect() { return abilityEffect; }
+    public void setAbilityIndex(Integer i) { this.abilityIndex = i; }
+    public void setAbilityLevel(Integer i) { this.abilityLevel = i; }
+    public void setAbilityClass(String s) { this.abilityClass = s; }
+    public void setAbilityName(String s) { this.abilityName = s; }
+    public void setAbilityDescription(String s) { this.abilityDescription = s; }
+    public void setAbilityDamage(String s) { this.abilityDamage = s; }
+    public void setAbilityEffect(String s) { this.abilityEffect = s; }
+}
